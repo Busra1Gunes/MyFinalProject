41b230db6f690103fc88aab0f09e35cc9ab45255
Büşra Güneş <busragns1999@hotmail.com>
2024-01-29 12:02:04 +0300

    29.01.2024 Products Controller düzenlemeleri yapıldı

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -23,13 +23,41 @@ namespace WebAPI.Controllers
             _productService = productService;
         }
 
-        [HttpGet]
-        public List<Product> Get()
+        [HttpGet("getall")]
+        public IActionResult Get()
         {
             //Dependency chain--Bağımlılık ziniciri
 
             var result = _productService.GetAll();
-            return result.Data;
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            //Dependency chain--Bağımlılık ziniciri
+
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("add")]
+        public IActionResult Post(Product product)
+        {
+            //Dependency chain--Bağımlılık ziniciri
+
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
